2ff027b714b547434b5047f45dc82073bc2fbc7c
Sofía Zapata Zavala <sofiazapatazavala@gmail.com>
2018-01-25 13:58:37 -0300

    Paso de 1.1 a 2.0

--- index.php
+++ index.php
@@ -1,87 +1,22 @@
-<?php get_header(); ?>
-<body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
 
-    <nav>
-    <div class="contenedor">
-
-        <?php wp_nav_menu(
-    array(
-        'container' => false,
-        'items_wrap' => '<ul id="menu" class="responsive menu space-between">%3$s</ul>',
-        'theme_location' => 'menu'
-        )); ?>
-
-        </div>
-    </nav>
-    <header id="inicio" class="flex-row">
-        <div class="amarillo" style="width: 2%"></div>
-        <div class="azul" style="width: 2%"></div>
-        <div class="morado" style="width: 4%"></div>
-            <h1 id="logo">Mesa Accion Aborto Chile </h1>
-
-        <div class="morado" style="width: 4%"></div>
-        <div class="azul" style="width: 2%"></div>
-        <div class="amarillo" style="width: 2%"></div>
-        </header>
-    <section id="quienes">
-        <div class="contenedor">
-            <h2 class="textoblanco underbarred">¿<span class="textoamarillo">Quiénes</span> Somos?</h2>
-            <ul class="responsive space-between">
-
-                <li class="flex-column">
-                    <img src="<?php bloginfo('template_url')?>/img/mujeres.png" />
-                    <h3>Somos Mujeres</h3>
-                    <p>La Mesa Acción por el Aborto en Chile se instala en 2015 con el objetivo de generar argumentos para el debate sobre la despenalización del aborto y el derecho a decidir de las mujeres. Este espacio está integrado por organizaciones de mujeres y feministas, organizaciones de derechos humanos, derechos sexuales y reproductivos como <a href="http://www.amnistia.cl">Amnistía Internacional </a>, <a href="http://www.aprofa.cl">APROFA</a>, Académicas de la Universidad de Chile y Universidad Diego Portales , <a href="http://www.humanas.cl">Corporación Humanas </a>, <a href="http://revoluciondemocratica.cl/frente-de-genero/">Frente de Género Revolución Democrática</a>, <a href="https://www.fondoalquimia.org">Fondo Alquimia , <a href="http://www.insmujer.cl">Fundación Instituto de la Mujer </a>, <a href="http://www.oge.cl">Observatorio de Género y Equidad</a>, <a href="http://www.observatoriogenerosalud.cl">Observatorio de Equidad de Género en Salud</a>, <a href="http://autonomascontralaviolencia.blogspot.cl">Coordinadora Autónoma Contra la Violencia</a>, <a href="https://www.facebook.com/IAFeminista/">Frente Feminista de Izquierda Autónoma</a></p>
-                </li>
-            </ul>
-        </div>
-    </section>
-    <section id="argumentos" style="background-image: url(<?php bloginfo('template_url')?>/img/megafono.png); background-position: -2% 102%; background-repeat: no-repeat;">
-        <div class="contenedor">
-            <h2 class="textogris underbarred"><span class="textorojo">Argumentos</span> para el Debate</h2>
-            <p class="descripcion">Entrevistas y Columnas de Opinión para <span class="bold">informarse y difundir</span></p>
-            <div class="responsive space-between">
-                <!-- <div><img src="<?php bloginfo('template_url')?>/img/megafono.png" /></div> -->
-
-                <?php query_posts('category_name=argumentos&posts_per_page=2'); ?>
-                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                <article class="articulo sombraroja">
-                  <?php if ( has_post_thumbnail() ) { ?>
-                    <?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
-                    <div class="cabecera" style="background-image: url('<?php echo $thumb['0'];?>')">
-                    </div>
-                  <?php } ?>
-                  <div class="contenido">
-                      <h4><?php the_title();?></h4>
-                      <?php the_excerpt();?><p><br>Publicado el <?php the_time('j') ?> de <?php the_time('F') ?> de <?php the_time('Y') ?></p>
-                      <a class="leermas" href="<?php the_permalink();?>">Leer más</a>
-                  </div>
-                </article>
-
-                <?php endwhile; else: ?>
-                <h3>No se encontraron Articulos</h3>
-                 <?php endif; ?>
-<?php wp_reset_query(); ?>
-
-            </div>
-            <a class="boton" href="http://www.accionaborto.cl/argumentos/">Ver más Argumentos</a>
-        </div>
-    </section>
-    <section id="herramientas">
-        <div class="contenedor">
-            <h2 class="textoblanco underbarwhite"><span class="textoamarillo">Herramientas</span> para la Acción</h2>
-            <p class="descripcion">Gráficas, GIFs, Infografías, Videos y Programas de Radio para <span class="bold">compartir</span></p>
-            <a class="boton" href="http://www.accionaborto.cl/herramientas/">Ir a Herramientas</a>
-            <a class="boton facebook" href="https://www.facebook.com/Mesa-Acci%C3%B3n-por-el-Aborto-en-Chile-219747685183613/">Ir a Facebook</a>
-        </div>
-    </section>
-    <section id="prensa">
-        <div class="contenedor">
-            <h2 class="textoblanco underbarred">Apariciones en <span class="textoamarillo">Prensa</span></h2>
-            <p class="descripcion">Noticias en Televisión, Radio, Medios Impresos y Digitales para <span class="bold">difundir</span></p>
-            <a class="boton" href="http://www.accionaborto.cl/prensa/">Ver links de prensa</a>
-        </div>
-    </section>
-<?php get_footer(); ?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
